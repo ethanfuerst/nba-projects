15ef8bc44be97f8bf4140b7208b2539893b82cf1
Ethan Fuerst <ethanfuerst@gmail.com>
2021-03-11 22:40:23 -0600

    created all star and playoff logs

---
+++ etls/shell_scripts/update_all_star_logs.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+export TZ="America/Chicago"
+TOP_LEVEL="$( cd .. && cd .. && pwd )"
+
+echo_time() {
+    date +"%d/%m/%Y %H:%M:%S%z $*"
+}
+
+source ${TOP_LEVEL}/env/bin/activate
+
+echo_time "starting script"
+python3 ${TOP_LEVEL}/etls/all_star_logs.py 
+echo_time "done running script"
+
+deactivate
+echo_time "Finished"
